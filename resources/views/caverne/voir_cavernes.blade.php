21b643274b5dce73ccbb0f4c1182008c24430622
Ypo <121876885+AxelBoulange@users.noreply.github.com>
2024-03-15 14:39:25 +0100

    template stable

--- resources/views/caverne/voir_cavernes.blade.php
+++ resources/views/caverne/voir_cavernes.blade.php
@@ -4,6 +4,10 @@
     action caverne
 @endsection
 
+@section('cavernes')
+    block py-2 px-3 text-red-400 bg-red-700 rounded md:bg-transparent md:p-0
+@endsection
+
 @section('body')
     <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
         <span class="font-medium">Danger alert!</span> voir caverne.

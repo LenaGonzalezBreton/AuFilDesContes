6ec66c941431610d26c672b94feac24cb378b9e8
Ypo <121876885+AxelBoulange@users.noreply.github.com>
2024-03-15 11:30:01 +0100

    web

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,4 @@ Route::get('/contes', function () {
 Route::get('/cavernes', function () {
     return view('caverne/voir_cavernes');
 })->name('cavernes');
+//dtyyeeyefyteyuectyevrtyvretiygrtrferetrgretrèigrebretuibrebtier

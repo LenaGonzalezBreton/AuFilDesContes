63e27fcff46680ee9366022c283834f321a42396
Ypo <121876885+AxelBoulange@users.noreply.github.com>
2024-04-09 14:43:28 +0200

    same

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,6 @@
  $('#popup-modal').hide();
-var idCaverne, idMotClef;
+var idCaverne;
+var idMotClef;
 
 function GetIdCaverne(id) {
 
@@ -42,12 +43,11 @@ function img_pathUrl(input){
     $('#img_url')[0].src = (window.URL ? URL : webkitURL).createObjectURL(input.files[0]);
 }
 
- function GetIdMotCle(id) {
-
+ function GetIdMotClef(id) {
 //   sessionStorage.setItem("id", id);
      idMotClef = id;
      console.log(idMotClef + '= idMotClef');
-     $('#popup-modal').modal('show');
+     $('#popup-modal').show();
  }
 
  function destroyMotClef(){
@@ -55,7 +55,7 @@ function img_pathUrl(input){
      const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
      // Envoi de l'ID au serveur via une requête fetch
-     fetch('/motcle/' + idMotClef, {
+     fetch('motcle/' + idMotClef, {
          method: 'DELETE',
          headers: {
              'Content-Type': 'application/json',
@@ -64,9 +64,11 @@ function img_pathUrl(input){
          body: JSON.stringify({id: idMotClef})
      })
          .then(response => {
+             console.log(response);
              if (response.ok) {
                  console.log('ID envoyé avec succès au serveur');
-                 location.reload();
+                 // document.getElementById(""popup-modal").style.display="none";
+                 document.querySelector('[data-modal-hide="popup-modal"]').click();
              } else {
                  console.error('Erreur lors de l\'envoi de l\'ID au serveur');
              }
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -53,9 +53,11 @@
                 @yield('body', 'Rien à afficher pour le moment.')
             </div>
         </div>
-        <script src="/assets/js/script.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+        <script src="{{asset('assets/js/script.js')}}"></script>
+
     </body>
 
 </html>
